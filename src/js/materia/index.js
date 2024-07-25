5044da1650c0ce068ac888a808bd270af353328d
edmoranm <edanielmoram@gmail.com>
2024-07-25 07:19:54 -0600

    creamos en materia.js con los eventos

---
+++ src/js/materia/index.js
@@ -0,0 +1,338 @@
+const btnGuardar = document.getElementById('btnGuardar')
+const btnModificar = document.getElementById('btnModificar')
+const btnBuscar = document.getElementById('btnBuscar')
+const btnCancelar = document.getElementById('btnCancelar')
+const btnLimpiar = document.getElementById('btnLimpiar')
+const tablaMaterias = document.getElementById('tablaMaterias')
+const formulario = document.querySelector('form')
+
+btnModificar.parentElement.style.display = 'none'
+btnCancelar.parentElement.style.display = 'none'
+
+const getMateria = async (alerta = 'si') => {
+    const nombre = formulario.mat_nombre.value.trim()
+    console.log(nombre)
+    
+    const url = `/tarea_crudj_proyecto_anterior/controladores/materia/index.php?mat_nombre=${nombre}`
+    const config = {
+        method: 'GET'
+    }
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        console.log(data);
+
+        tablaMaterias.tBodies[0].innerHTML = ''
+        const fragment = document.createDocumentFragment()
+        let contador = 1;
+        
+        if (respuesta.status == 200) {
+            if(alerta == 'si'){
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "success",
+                title: 'Materias encontradas',
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+            }
+
+            if (data.length > 0) {
+                data.forEach(materias => {
+                    const tr = document.createElement('tr')
+                    const celda1 = document.createElement('td')
+                    const celda2 = document.createElement('td')
+                    const celda3 = document.createElement('td')
+                    const celda4 = document.createElement('td')
+                    const buttonModificar = document.createElement('button')
+                    const buttonEliminar = document.createElement('button')
+
+                    celda1.innerText = contador;
+                    celda2.innerText = materias.mat_nombre;
+
+
+                    buttonModificar.textContent = 'Modificar'
+                    buttonModificar.classList.add('btn', 'btn-warning', 'w-100')
+                    buttonModificar.addEventListener('click', () => llenardatos(materias))
+
+                    buttonEliminar.textContent = 'Eliminar'
+                    buttonEliminar.classList.add('btn', 'btn-danger', 'w-100')
+                    buttonEliminar.addEventListener('click', () => Eliminar(materias.mat_id));
+
+
+                    celda3.appendChild(buttonModificar)
+                    celda4.appendChild(buttonEliminar)
+
+                    tr.appendChild(celda1)
+                    tr.appendChild(celda2)
+                    tr.appendChild(celda3)
+                    tr.appendChild(celda4)
+                    tr.appendChild(celda5)
+                    tr.appendChild(celda6)
+                    tr.appendChild(celda7)
+                    tr.appendChild(celda8)
+                    fragment.appendChild(tr);
+
+                    contador++
+                });
+
+            } else {
+                const tr = document.createElement('tr')
+                const td = document.createElement('td')
+                td.innerText = 'No hay materias disponibles'
+                td.colSpan = 8;
+
+                tr.appendChild(td)
+                fragment.appendChild(tr)
+            }
+        } else {
+            console.log('error al cargar');
+        }
+
+        tablaMaterias.tBodies[0].appendChild(fragment)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getMateria();
+
+
+const guardarMaterias = async (e) => {
+    e.preventDefault();
+    btnGuardar.disabled = true;
+
+    const url = '/tarea_crudj_proyecto_anterior/controladores/materias/index.php'
+    const formData = new FormData(formulario)
+    formData.append('tipo', 1)
+    formData.delete('mat_id')
+    const config = {
+        method: 'POST',
+        body: formData
+    }
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        const { mensaje, codigo, detalle } = data
+        Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: "success",
+            title: mensaje,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        }).fire();
+
+        alert(mensaje)
+        console.log(data);
+
+        if (codigo == 1 && respuesta.status == 200) {
+            getMateria(alerta = 'no');
+            //formulario.reset();
+        } else {
+            console.log(detalle);
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+    btnGuardar.disabled = false;
+}
+
+
+
+const llenardatos = (materias) => {
+
+    formulario.mat_id.value = materias.mat_id
+    formulario.mat_nombre.value = materias.mat_nombre
+    btnBuscar.parentElement.style.display = 'none'
+    btnGuardar.parentElement.style.display = 'none'
+    btnLimpiar.parentElement.style.display = 'none'
+    btnModificar.parentElement.style.display = ''
+    btnCancelar.parentElement.style.display = ''
+
+}
+
+const cancelar = (materias) => {
+
+    formulario.mat_id.value = ''
+    formulario.mat_nombre.value = ''
+    btnBuscar.parentElement.style.display = ''
+    btnGuardar.parentElement.style.display = ''
+    btnLimpiar.parentElement.style.display = ''
+    btnModificar.parentElement.style.display = 'none'
+    btnCancelar.parentElement.style.display = 'none'
+
+}
+
+const modificar = async(e) => {
+    e.preventDefault();
+    btnModificar.disabled = true;
+
+    const url = '/tarea_crudj_proyecto_anterior/controladores/materias/index.php';
+    const formData = new FormData(formulario);
+    formData.append('tipo', 2);
+    formData.append('mat_id', formulario.mat_id.value);
+    const config = {
+        method: 'POST',
+        body: formData
+    };
+
+    try {
+        console.log('Enviando datos:', ...formData.entries());
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        console.log('Respuesta recibida:', data);
+        const { mensaje, codigo, detalle } = data;
+        if (respuesta.ok && codigo === 1) {
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "success",
+                title: mensaje,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+            formulario.reset()
+            getMateria(alerta = 'no');
+            btnBuscar.parentElement.style.display = ''
+            btnGuardar.parentElement.style.display = ''
+            btnLimpiar.parentElement.style.display = ''
+            btnModificar.parentElement.style.display = 'none'
+            btnCancelar.parentElement.style.display = 'none'
+         
+        } else {
+            console.log('Error:', detalle);
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "error",
+                title: 'Error al guardar',
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+        }
+    } catch (error) {
+        console.log('Error de conexión:', error);
+        Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: "error",
+            title: 'Error de conexión',
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        }).fire();
+    }
+    btnModificar.disabled = false;
+    btnCancelar.disabled = false;
+
+
+}
+
+const Eliminar = async(mat_id) => {
+    const url = '/tarea_crudj_proyecto_anterior/controladores/materias/index.php';
+    const formData = new FormData();
+    formData.append('tipo', 3);
+    formData.append('mat_id', mat_id);
+    const config = {
+        method: 'POST',
+        body: formData
+    };
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        console.log('Respuesta recibida:', data);
+        const { mensaje, codigo, detalle } = data;
+        if (respuesta.ok && codigo === 1) {
+            Swal.mixin({
+                toast: true,
+                position: "top-start",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "success",
+                title: mensaje,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+            getMateria(alerta = 'no');
+            btnBuscar.parentElement.style.display = ''
+            btnGuardar.parentElement.style.display = ''
+            btnLimpiar.parentElement.style.display = ''
+            btnModificar.parentElement.style.display = 'none'
+            btnCancelar.parentElement.style.display = 'none'
+         
+        } else {
+            console.log('Error:', detalle);
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "error",
+                title: 'Error al guardar',
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+        }
+    } catch (error) {
+        console.log('Error de conexión:', error);
+        Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: "error",
+            title: 'Error de conexión',
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        }).fire();
+    }
+    btnModificar.disabled = false;
+    btnCancelar.disabled = false;
+
+
+}
+
+
+formulario.addEventListener('submit', guardarMaterias)
+btnBuscar.addEventListener('click', getMateria)
+btnModificar.addEventListener('click', modificar)
+btnCancelar.addEventListener('click', cancelar)
